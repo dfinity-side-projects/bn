645379f3aab6386bf5f490104584b4a91344ab56
Paul H. Liu <paul@dfinity.org>
2018-03-28 16:26:44 -0700

    Fix nix installation of dylib on Mac

--- bn.nix
+++ bn.nix
@@ -4,5 +4,11 @@ stdenv.mkDerivation {
   version="0.0.0";
   src = ./.;
   buildInputs = [ gmpxx openssl llvm ];
-  installPhase = "make PREFIX=$out/ install";
+  installPhase = 
+    "make PREFIX=$out/ install;" +
+    stdenv.lib.optionalString stdenv.isDarwin ''
+      install_name_tool -change lib/libmcl.dylib $out/lib/libmcl.dylib $out/lib/libbls384.dylib
+      install_name_tool -id $out/lib/libbls384.dylib $out/lib/libbls384.dylib
+      install_name_tool -id $out/lib/libmcl.dylib $out/lib/libmcl.dylib
+    '';
 }
